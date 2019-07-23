c4520daf093524a05325e976cad2fa2e87946b06
bigfang <bitair@gmail.com>
2019-07-23 15:01:25 +0800

    hello world

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('hello ts')
+import request, { Response } from 'request'
+
+const url =
+  'http://api.dataide.eastmoney.com/data/gethglist?pageindex=1&pagesize=830&orderby=dim_scode'
+
+request.get(
+  'https://baidu.com',
+  (_err: any, _resp: Response, body: any): any => {
+    console.log(body)
+  }
+)
