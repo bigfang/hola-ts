b6609febacf65195c3cb24b56b3bd88b56126c2e
bigfang <bitair@gmail.com>
2019-07-23 17:34:37 +0800

    can debug

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import axios from 'axios'
 
 const opts = {
   pageindex: 1,
-  pagesize: 20,
+  pagesize: 2,
   orderby: 'dim_scode',
 }
 const url = 'http://api.dataide.eastmoney.com/data/gethglist'
 
-axios.get(url, { params: opts }).then(resp => {
+axios.get(url, { params: opts }).then((resp): void => {
   console.log(resp.data.data)
 })
